f8f2f139735eb9838ca065d3049c0520904e3c45
Zachary <zvcoding@gmail.com>
2022-03-25 13:02:42 -0400

    pretty hard road block but we will get over it, the character is showing late

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,11 +10,16 @@ function App() {
 
   const [hiraganas, setHiragana] = useState(null);
   const [allSoundCats,setAllSoundCats] = useState(null);
+  const [min, setMin] = useState(0)
+  const [max, setMax] = useState(10)
+  const [arrayOfNumbers, setNumbers] = useState(null)
+  const [numOfCards, setNumberOfCards] = useState(10)
 
   useEffect(()=>init(),[]);
 
   function init(){
     getSoundCats()
+    handleID();
     getHiragana()
   }
 
@@ -31,6 +36,19 @@ function App() {
     .catch(error => console.error(error));
   }
 
+  const cards = () => [...Array(numOfCards)].map((_, i) => randomIntFromInterval(min,max));
+
+  function randomIntFromInterval(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  };
+
+  const handleID = () =>{
+    setNumbers(cards())
+  }
+
+
+
+
   
 
   return (
@@ -46,7 +64,13 @@ function App() {
           </Route>
 
           <Route path="/game">
-            {hiraganas?<Game hiraganas ={hiraganas} allSoundCats = {allSoundCats}/>:null}
+            {hiraganas?
+            <Game
+              hiraganas ={hiraganas}
+              allSoundCats = {allSoundCats}
+              arrayOfNumbers ={arrayOfNumbers}
+              numOfCards = {numOfCards}
+            />:null}
           </Route>
 
         </Switch>
